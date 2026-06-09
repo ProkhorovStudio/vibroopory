3d5467fae1d0e3970b846651b2f0e7f58aa3ebda
Аккаунт Хостинга Виброопоры <g50941po_prog@zippo.beget.ru>
2026-06-09 12:53:17 +0300

    С боя скрипты

--- bitrix/templates/aspro_next/header.php
+++ bitrix/templates/aspro_next/header.php
@@ -102,34 +102,37 @@ if(Loader::IncludeModule('prokhorov.trafic'))
         	}
 
         ?>
-        <script type="text/javascript" >
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                m[i].l=1*new Date();
-                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    <? if(SITE_SERVER_NAME != 'vibrobot.ru'):?>
+            <script type="text/javascript" >
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();
+                    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                    k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-            ym(54700726, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                params:{ip:'<?=$user_ip?>'},
-                webvisor:true,
-                ecommerce:"dataLayer"
-            });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/54700726" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        <!-- /Yandex.Metrika counter -->
-        <!-- Roistat Counter Start -->
-        <script>
-            (function(w, d, s, h, id) {
-                w.roistatProjectId = id; w.roistatHost = h;
-                var p = d.location.protocol == "https:" ? "https://" : "http://";
-                var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
-                var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
-            })(window, document, 'script', 'cloud.roistat.com', '6009b5ffb5e37e6cc67a7befe34f5760');
-        </script>
-        <!-- Roistat Counter End -->
+                ym(54700726, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    params:{ip:'<?=$user_ip?>'},
+                    webvisor:true,
+                    ecommerce:"dataLayer"
+                });
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/54700726" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+            <!-- /Yandex.Metrika counter -->
+            <!-- Roistat Counter Start -->
+            <script>
+                (function(w, d, s, h, id) {
+                    w.roistatProjectId = id; w.roistatHost = h;
+                    var p = d.location.protocol == "https:" ? "https://" : "http://";
+                    var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+                    var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+                })(window, document, 'script', 'cloud.roistat.com', '6009b5ffb5e37e6cc67a7befe34f5760');
+            </script>
+            <!-- Roistat Counter End -->
+    <?endif;?>
+
         <?else:?>
             <?$APPLICATION->IncludeComponent(
             "ldo:checkCaptcha",
