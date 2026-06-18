80a60c96bc4164c772d607adfe8a899394965e7d
Аккаунт Хостинга Виброопоры <g50941po_prog@zippo.beget.ru>
2026-06-18 16:57:18 +0300

    С боя после установки нового счетчика

--- bitrix/templates/aspro_next/header.php
+++ bitrix/templates/aspro_next/header.php
@@ -115,14 +115,15 @@ if(Loader::IncludeModule('prokhorov.trafic'))
             <!-- Roistat Counter Start -->
             <script>
                 (function(w, d, s, h, id) {
-                    w.roistatProjectId = id; w.roistatHost = h;
+                    w.roistatProjectId = id; w.roistatHost = h; w.roistatPage = d.location.href; w.roistatReferrer = d.referrer;
                     var p = d.location.protocol == "https:" ? "https://" : "http://";
                     var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
                     var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
                 })(window, document, 'script', 'cloud.roistat.com', '6009b5ffb5e37e6cc67a7befe34f5760');
             </script>
             <!-- Roistat Counter End -->
-    <?endif;?>
+
+        <?endif;?>
 
         <?else:?>
             <?$APPLICATION->IncludeComponent(
